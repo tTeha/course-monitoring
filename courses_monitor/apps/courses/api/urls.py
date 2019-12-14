40c13a1a29431bf528f37f6492c52b4792188394
TEha1 <abdelfatah.ramadan42@gmail.com>
2019-12-14 03:05:17 +0200

    Update urls.py

--- courses_monitor/apps/courses/api/urls.py
+++ courses_monitor/apps/courses/api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('courses/', include(r.urls)),
     path('teacher/<int:pk>/courses/', views.CourseLCView.as_view(), name="teacher_courses"),
     path('teacher/<int:spk>/course/<int:pk>/', views.CourseRUDView.as_view(), name="RUD_teacher_courses"),
-    path('course/<int:pk>/matrials/', views.CourseMaterialsCreateView.as_view(), name="teacher_courses_materials"),
+    path('course/<int:pk>/materials/', views.CourseMaterialsCreateView.as_view(), name="teacher_courses_materials"),
     path('student/<int:pk>/studentcourses/', views.EnrollCoursesCreateView.as_view(), name="add_student_courses"),
     path('student/<int:spk>/studentcourse/<int:pk>/', views.EnrollCoursesRUDView.as_view(), name="RD_student_courses"),
     path('student/<int:pk>/courses/', views.StudentCoursesListView.as_view(), name="get_a_student_courses"),
